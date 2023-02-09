8c0c05808bbdd053ea75804281e38086bcd6a901
siddevi <121307234+siddevi@users.noreply.github.com>
2023-02-09 11:33:32 +0000

    update the server url

--- server/index.js
+++ server/index.js
@@ -21,9 +21,12 @@ app.post("/", async (req, res) => {
   const { message } = req.body;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: message,
-    max_tokens: 3000,
-    temperature: 0.3,
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
   });
   res.json({ botResponse: response.data.choices[0].text });
 });
